4926b4b878c9bfa07108a6d67e9e945e102282bf
Edenik <Edenik5@gmail.com>
2020-10-07 19:20:00 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flutter Send Whatsapp App
+
+
+This is a demo app used to send whatsapp message to unrecognized phone number without saving user's number.
+
---
+++ android/app/src/main/kotlin/com/example/flutter_send_whatsapp_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_send_whatsapp_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
